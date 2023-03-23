94dd65803c15668b9215cebbbec9702d91ebf016
solracss <herekespanol@gmail.com>
2023-03-23 18:51:24 +0100

    Change interface type

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -16,7 +16,7 @@ public class UserController : Controller
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    public async Task<ActionResult<ICollection<User>>> GetUsers()
     {
         var usersDtos = await userService.GetAllUsersAsync();
         return Ok(usersDtos);
