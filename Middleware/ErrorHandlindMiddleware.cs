a7c285355a1f6261ef81bbbd15de8a9c6b893337
solracss <herekespanol@gmail.com>
2023-03-24 11:00:09 +0100

    Add error middleware

---
+++ Middleware/ErrorHandlindMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using ReservationAPI.Exceptions;
+
+namespace ReservationAPI.Middleware
+{
+    public class ErrorHandlindMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (NotFoundException notFoundException)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFoundException.Message);
+            }
+        }
+    }
+}
